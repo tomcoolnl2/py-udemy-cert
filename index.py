08ec53ed96776b272eba154a40f592e17dd11ac5
coolt <tom.cool@tomtom.com>
2022-06-25 09:24:53 +0200

    nitial commit

---
+++ index.py
@@ -0,0 +1,25 @@
+# my_list = ['b', 'd', 'a', 'z', 'x']
+# another_list = [1, 2, 3, 4, 5]
+
+# my_list = my_list[:3]
+
+# my_list.sort(reverse=True)
+
+# print(my_list)
+
+
+# #  ['d', 'b', 'a', 3, 2, 1]
+
+# Assignment 1:
+"""
+Print Bill's salary from the my_list object shown below.
+
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
+
+"""
+
+
+# your code below:
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'TV']]
+
+print(my_list[0]['Bill'])
